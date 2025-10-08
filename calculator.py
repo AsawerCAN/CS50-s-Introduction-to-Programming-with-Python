faa6854558cccafedec920e3d1808b50415135eb
Asawer Naseer <muhammadasawer75@gmail.com>
2025-10-09 00:52:30 +0500

    first commit

---
+++ calculator.py
@@ -0,0 +1,23 @@
+#int
+# x = int(input("what's x "))
+# y = int(input("what's y "))
+
+# print(x + y)
+
+# print(int(input("what's x ")) + int(input("what's y ")))
+
+#float
+# print(float(input("what's x ")) + float(input("what's y ")))
+
+x = float(input("what's x "))
+y = float(input("what's y "))
+
+# z = round(x + y)
+
+# print(f"{z:,}")
+
+# z = round(x / y, 2)
+# print(z)
+
+z = (x / y)
+print(f"{z:.2f}")
---
+++ hello.py
@@ -0,0 +1,49 @@
+#str
+#Ask user their name & Remove whitespace from str and capitalize user's name
+# name = input("What's your name? ").strip().title()
+
+#print their name 
+# print("Hello",name)
+
+# def hello(to="world"):
+#   print("Hello", to)
+
+# hello()
+# name = input("What's your name? ").strip().title()
+# hello(name)
+
+
+# def main():
+#   name = input("What is your name? ")
+#   hello(name)
+
+
+# def hello(to="world"):
+#   print("Hello", to)
+
+# main()
+
+# def main():
+#   x = int(input("Enter value of x "))
+#   print("Square of x is:", square(x))
+
+# def square(n):
+#   return n * n
+#   # return n **2 
+#   # return pow(n, 2)
+
+# main()
+
+'''Question: Write a function named add_and_subtract that takes two numbers as arguments, a and b. The function should return the sum of the two numbers and the difference (a - b) as a tuple.
+
+Example:
+If you call add_and_subtract(10, 3), it should return (13, 7).'''
+
+# def add_and_Subtract(a,b):
+#   sum = a + b
+#   difference = a - b
+#   return (sum, difference)
+
+
+# x = add_and_Subtract(10,3)
+# print(x)
